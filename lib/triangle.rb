82091f371c7df1179a584e3cf7769dd46a42a899
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-20 20:05:30 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,70 @@
+require 'pry'
 class Triangle
-  # write code here
+  
+  attr_accessor :length1, :length2, :length3
+  
+  
+  
+  def initialize(length1,length2,length3)
+    @all=[]
+    @length1=length1
+    @length2=length2
+    @length3=length3
+    @all << @length1
+    @all << @length2
+    @all << @length3
+    @x=@all[0]
+    @y=@all[1]
+    @z=@all[2]
+  end 
+  
+  def all_equal?(arr)
+    arr.uniq.size <=1 
+  end 
+  
+  def two_equal?(arr)
+    @length1==@length2 || @length1==@length3 || @length2==@length3
+  end 
+  
+  
+  
+  def kind 
+    
+    if @x.positive?() && @y.positive?() && @z.positive?() && @x+@y >@z && @y+@z>@x && @x+@z=@y 
+       
+      if all_equal?(@all)
+      return :equilateral
+      
+      elsif two_equal?(@all)
+      return :isosceles
+    
+      else 
+        return :scalene
+      end
+   
+    else 
+      begin
+      
+        raise TraingleError
+      rescue TraingleError => error
+          puts error.message
+     
+      end
+    end 
+  
+  end 
+ 
+  class TriangleError < StandardError
+    def message
+      "You must give valid length for each side of the trainlge"
+      
+    end
+  end 
 end
+    
+    
+  
+ 
+  
+  
+
